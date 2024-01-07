ac4a7914af5292b46850abe8629a58342bb74e2b
VictorMA18 <vmamanian@unsa.edu.pe>
2024-01-07 03:22:01 +0000

    Prueba hecha donde podemos verificar que el resultado de nuestro cgi el cual era poner a un actor con in id de 5 este nos muestre este resultado ya que lo pudimos completar entonces nos faltaria el html

--- Lab10/cgi-bin/Primero.pl
+++ Lab10/cgi-bin/Primero.pl
@@ -11,11 +11,21 @@ my $password = "pweb1";
 my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.0.10";
 my $dbh = DBI->connect($dsn, $user, $password) or die("No se conecto");
 my $idactor = "5";
-my $sth = $dbh -> prepare("SELECT * FROM Actor WHERE ActorId<100");
+my $sth = $dbh->prepare("INSERT INTO Actor (ActorId, Name) Values(?,?)");
+$sth->execute($idactor, "Pancho");
 $sth = $dbh->prepare("DESC Actor");
 $sth->execute();
 while(my @rowcabe = $sth->fetchrow_array){
-   print "$rowcabe[0]\n";
+   print "$rowcabe[0] ";
 }
-$sth -> finish;
+$sth = $dbh -> prepare("SELECT * FROM Actor WHERE ActorId=?");
+$sth->execute($idactor);
+while(my @row = $sth->fetchrow_array){
+  print "\n";
+  foreach my $data(@row){
+    print "$data ";
+  }
+  print "\n";
+}
+$sth->finish;
 $dbh->disconnect;
